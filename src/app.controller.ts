e8e8f3abac351f1a91352c64b7f6fda707e1612d
ordinaryask <ordinaryask@gmail.com>
2022-10-03 03:00:46 +0200

    feat: add swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,30 @@
 import { Controller, Get, Param } from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 
 import { LocaleJSON, Locale, Namespace } from './types'
 import { AppService } from './app.service'
 
+@ApiTags('App Controller')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Get all namespaces' })
+  @ApiResponse({ status: 200, type: [String] })
   @Get('/namespaces')
-  getNamespaces(): string[] {
+  getNamespaces(): Namespace[] {
     return Object.values(Namespace)
   }
 
+  @ApiOperation({ summary: 'Get all locales' })
+  @ApiResponse({ status: 200, type: [String] })
   @Get('/locales')
   getLocales(): string[] {
     return Object.values(Locale)
   }
 
+  @ApiOperation({ summary: 'Get resource' })
+  @ApiResponse({ status: 200 })
   @Get('/:locale/:namespace')
   getResourse(
     @Param('locale') locale: Locale,
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function server() {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule, { cors: true })
+
+  const config = new DocumentBuilder()
+    .setTitle('localization')
+    .setDescription('Documentation')
+    .setVersion('1.0.0')
+    .build()
+  const documentation = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('/docs', app, documentation)
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
